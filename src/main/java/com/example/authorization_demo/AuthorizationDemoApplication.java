f8e95fb70d45d1238e25893cff5081b4029a8426
Amit@1993 <eramit3993@gmail.com>
2023-06-22 08:33:35 +0530

    .

--- src/main/java/com/example/authorization_demo/AuthorizationDemoApplication.java
+++ src/main/java/com/example/authorization_demo/AuthorizationDemoApplication.java
@@ -11,6 +11,7 @@ public class AuthorizationDemoApplication {
 
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(AuthorizationDemoApplication.class, args);
 	}
 
